816d52336ea92b92b9cdafabfec077cec5852951
hy <2327401139@qq.com>
2022-11-08 15:04:18 +0800

    初始化项目并创建付款模块

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hy 2327401139@qq.com
+ * @version 1.0 2022/11/8 13:25
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}

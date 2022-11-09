a87561da23af4c14cb19800d8b48e7269abc2493
huyan <2327401139@qq.com>
2022-11-10 00:04:58 +0800

    将Order、Payment模块注册到Eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,16 +2,16 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hy 2327401139@qq.com
  * @version 1.0 2022/11/9 21:20
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
     }
-
 }

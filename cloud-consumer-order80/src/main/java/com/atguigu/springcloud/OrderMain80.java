005ccefcd96e10bd85354caf938a24a27de2f218
huyan <2327401139@qq.com>
2022-11-09 22:00:25 +0800

    添加消费者Order模块

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hy 2327401139@qq.com
+ * @version 1.0 2022/11/9 21:20
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+
+}
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -26,21 +23,23 @@ public class PaymentController {
     /**
      * 这里之前 我都是会在参数 Payment payment 前加 @RequestBody 的这样前端需要构建一个body体才可以
      * 现在这里没有，我看老师直接使用 效果和 @RequestParam 一样
-     *
+     * 2022年11月9日 21点48分 发现了，如果不加 @RequestBody 的话，消费者调用的时候，由于使用的get请求，无法发送body
+     *                      内部用的restTemplate，它的第二个参数其实就是body体的内容，其实这属于是转发了下请求就需要按照转发的那个人要求。
+     *                      如果在第一个参数url中也写上?serial=" + payment.getSerial() 的话，也是可以正常添加的,但这没必要。
      *
      * 有一个bug，在mapper.xml设置了useGeneratedKeys但是这里的result还是返回改变的行数
      * https://juejin.cn/post/7101544174394015757
      * 原来真正的id已经被注入到传参对象的主键对应属性里了，只需要使用插入语句的入参对象的get方法即可获取到正确的自增id
      */
     @PostMapping("/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
 
         log.info("*****插入结果：" + result);
 
         if (result > 0) {
-            //return new CommonResult(200, "添加成功", payment.getId()); 显示新添加的ID而不是改变行数。
-            return new CommonResult(200, "添加成功", result);
+            return new CommonResult(200, "添加成功", payment.getId()); // 显示新添加的ID而不是改变行数。
+            //return new CommonResult(200, "添加成功", result);
         } else {
             return new CommonResult(444, "添加失败");
         }

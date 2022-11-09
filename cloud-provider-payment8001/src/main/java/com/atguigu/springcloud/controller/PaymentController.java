028315b3ba5abd54853df83af46a2b13180d6641
hy <2327401139@qq.com>
2022-11-09 10:13:31 +0800

    date改为data、解决create返回不是新ID而是改变行数的问题

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -26,6 +26,11 @@ public class PaymentController {
     /**
      * 这里之前 我都是会在参数 Payment payment 前加 @RequestBody 的这样前端需要构建一个body体才可以
      * 现在这里没有，我看老师直接使用 效果和 @RequestParam 一样
+     *
+     *
+     * 有一个bug，在mapper.xml设置了useGeneratedKeys但是这里的result还是返回改变的行数
+     * https://juejin.cn/post/7101544174394015757
+     * 原来真正的id已经被注入到传参对象的主键对应属性里了，只需要使用插入语句的入参对象的get方法即可获取到正确的自增id
      */
     @PostMapping("/payment/create")
     public CommonResult create(Payment payment) {
@@ -34,6 +39,7 @@ public class PaymentController {
         log.info("*****插入结果：" + result);
 
         if (result > 0) {
+            //return new CommonResult(200, "添加成功", payment.getId()); 显示新添加的ID而不是改变行数。
             return new CommonResult(200, "添加成功", result);
         } else {
             return new CommonResult(444, "添加失败");
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -15,7 +15,7 @@ public class CommonResult<T> {
 
     private Integer code;
     private String message;
-    private T date;
+    private T data;
 
     public CommonResult(Integer code, String message) {
         this(code, message, null);
